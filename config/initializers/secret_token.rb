b418f58f3f5d4c9c4fd3ffbcdd167ed7d14f1e2e
Andres F. Lopez <afla@rolem.co>
2013-10-16 00:29:40 -0500

    v0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lol::Application.config.secret_token = 'abd8ae4d3b4e845147256c1c776f03a276021dc45437ca85c3526b30c8d1430ea2a1e8a78667357ad3ba43108f906165d5287fd94eea16bc48fdd222ef57fb9b'
---
+++ db/migrate/20131016020442_create_reports.rb
@@ -0,0 +1,12 @@
+class CreateReports < ActiveRecord::Migration
+  def change
+    create_table :reports do |t|
+      t.string :summoner
+      t.string :type
+      t.string :reason
+      t.text :detail
+
+      t.timestamps
+    end
+  end
+end
