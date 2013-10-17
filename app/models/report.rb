a5ef2be4e59ce3b9d09bfcd6a8db96e4ce2dd69a
Andres F. Lopez <afla@rolem.co>
2013-10-17 11:56:44 -0500

    Version1 Andes

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report < ActiveRecord::Base
   attr_accessible :detail, :reason, :summoner, :types
-  attr_accessor :title
-	validates_presence_of :reason, :summoner, :types
+
+  validates_presence_of :reason, :summoner, :types
 end
