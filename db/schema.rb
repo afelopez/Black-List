d1a0bc6d14433e3e096b8d5ec8667ed2f8e18df4
Andres L. Arbelaez <ingrato@ubuntu.(none)>
2013-10-29 22:50:34 -0500

    hacked:S

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(:version => 20131029141600) do
     t.string   "types"
     t.string   "reason"
     t.text     "detail"
-    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
