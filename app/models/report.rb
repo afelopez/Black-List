1eace309664f5ae47e4d77815031a684ec979cc4
Andres L. Arbelaez <ingrato@ubuntu.(none)>
2013-10-31 02:03:30 -0500

    lol

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,25 @@
+class CountPerDayValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    count = record.class.where(attribute => value).count
+    record.errors[attribute] << "reportado muchas veces en esta hora" if count >= options[:max]
+	tiempo = 10
+	if count >= options[:max]
+		
+    
+    	
+    end
+  end
+end
+
 class Report < ActiveRecord::Base
   attr_accessible :detail, :reason, :summoner, :types , :email
 
-  validates_presence_of :reason, :summoner, :types
+  validates_presence_of :reason, :types
+
+  validates :summoner, :presence => true,
+                   :length => { :maximum => 20 }
+
+ validates :summoner, :count_per_day => {:max => 4}
+
+
 end
