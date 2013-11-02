d84f61dbb4ac31c1489e3e13c3cc19fa5fbe9e14
Andres L. Arbelaez <ingrato@ubuntu.(none)>
2013-11-02 11:50:01 -0500

    contador

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,8 @@
 class CountPerDayValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-
+    last = Time.now
     time = Time.now
-    if time.min - last.min < 40 
+    if time.min - last.min < 1 
     count = record.class.where(attribute => value).count
     record.errors[attribute] << "reportado muchas veces en esta hora" if count >= options[:max]
     end
@@ -12,12 +12,16 @@ end
 
 
 class Report < ActiveRecord::Base
-  attr_accessible :detail, :reason, :summoner, :types , :email
+  attr_accessible :detail, :reason, :summoner, :types , :email , :last
 
   validates_presence_of :reason, :types, :summoner
 
   validates :summoner, :presence => true,
                    :length => { :maximum => 20 }
   validates :summoner, :count_per_day => {:max => 4}
-  last = Time.now
+
+  after_create :metodo
+  def metodo
+    last = Time.now
+  end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -2,9 +2,4 @@
 RAILS_ROOT = File.dirname(__FILE__) + '/..'
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
-#
-every 1.minutes do
-  runner "CountPerDayValidator.validate_each"
-  
-end
 
