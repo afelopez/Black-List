0121b48440a04ff3ab4a5047ab4f5e16bf95d849
Andres L. Arbelaez <ingrato@ubuntu.(none)>
2013-11-03 10:56:46 -0500

    count

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,27 +1,29 @@
 class CountPerDayValidator < ActiveModel::EachValidator
+
   def validate_each(record, attribute, value)
-    last = Time.now
+
     time = Time.now
-    if time.min - last.min < 1 
+    t = Report.new()
     count = record.class.where(attribute => value).count
-    record.errors[attribute] << "reportado muchas veces en esta hora" if count >= options[:max]
-    end
+      if count >= options[:max]
+        if  time.min - t.lol.min < 1
+            record.errors[attribute] << "cant be report more times"
+        end
+      end 
   end
 end
 
 
 
 class Report < ActiveRecord::Base
-  attr_accessible :detail, :reason, :summoner, :types , :email , :last
-
-  validates_presence_of :reason, :types, :summoner
-
-  validates :summoner, :presence => true,
-                   :length => { :maximum => 20 }
-  validates :summoner, :count_per_day => {:max => 4}
-
-  after_create :metodo
-  def metodo
-    last = Time.now
+  attr_accessible :detail, :reason, :summoner, :types , :email , :last 
+  validates_presence_of  :summoner #,:reason, :types
+  validates_length_of :summoner, :minimum => 4, :maximum => 20, :allow_blank => true
+  validates :summoner, :count_per_day => {:max => 1}
+  before_save do :lol
+    def lol
+      lastsave = Time.now
+    end
   end
 end
+
