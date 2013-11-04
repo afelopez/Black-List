e8f05cdd736da61d4a4e6a7a0fb575be431b51c3
Andres L. Arbelaez <ingrato@ubuntu.(none)>
2013-11-04 14:48:57 -0500

    success

--- app/models/la.rb
+++ app/models/la.rb
@@ -1,25 +1,27 @@
 class CountPerDayValidator < ActiveModel::EachValidator
 
-  def validate_each(record, attribute, value)
+  def validate_each(record, attribute, value) 
 
+    ultimo = Time.now - Time.now.min - 50
     time = Time.now
-    t = La.new()
-    count = record.class.where(attribute => value).count
-      if count >= options[:max]
-        if  time.min - t.lol.min < 1
+    lol = La.find_by_summoner(param[:summoner])
+    summ = La.where(["created_at >= ? AND created_at <= ?", ultimo, time]).count  
+    if summ >= 4
+      count = record.class.where(attribute => value).count
+        if count >= options[:max]
             record.errors[attribute] << "cant be report more times"
         end
-      end 
+    end
   end
 end
 
 
+
 class La < ActiveRecord::Base
   attr_accessible :detail, :reason, :summoner, :types , :email , :last 
-  validates_presence_of  :reason, :types
+ # validates_presence_of  :reason, :types
   validates_length_of :summoner, :minimum => 4, :maximum => 20, :allow_blank => true
-  validates :summoner, :count_per_day => {:max => 1}    
-  def lol
-    last = Time.now
-  end 
+  validates :summoner, :count_per_day => {:max => 4}
+    
 end
+
