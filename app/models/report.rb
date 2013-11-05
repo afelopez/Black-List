2e13e2b013b6658466e61b9c75ef2d08117ba368
Andres L. Arbelaez <ingrato@ubuntu.(none)>
2013-11-04 21:07:01 -0500

    LOGRADO

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,11 +5,12 @@ class CountPerDayValidator < ActiveModel::EachValidator
     
     
       count = record.class.where(attribute => value).count
+      omg = record.class.where(attribute => value)
         if count >= options[:max]
-          ultimo = Time.now - 60
+          ultimo = Time.now - 2400
           time = Time.now
-          summ = Report.where(["created_at >= ? AND created_at <= ?", ultimo, time]).count  
-          if summ >= 4
+          summ = omg.where(["created_at >= ? AND created_at <= ?", ultimo, time]).count  
+          if summ >= 5
             record.errors[attribute] << "cant be report more times "
           end
         end
@@ -20,9 +21,9 @@ end
 
 class Report < ActiveRecord::Base
   attr_accessible :detail, :reason, :summoner, :types , :email , :last 
- # validates_presence_of  :reason, :types
+  validates_presence_of  :reason, :types
   validates_length_of :summoner, :minimum => 4, :maximum => 20, :allow_blank => true
-  validates :summoner, :count_per_day => {:max => 4}
+  validates :summoner, :count_per_day => {:max => 5}
     
 end
 
